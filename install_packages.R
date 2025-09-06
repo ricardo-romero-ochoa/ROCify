a7a752fde47cfbf2c758b04a15f6ea5239a440d1
Ricardo Romero <62968631+ricardo-romero-ochoa@users.noreply.github.com>
2025-09-06 15:14:39 -0600

    Add files via upload

--- install_packages.R
+++ install_packages.R
@@ -1,3 +1,4 @@
-install.packages("BiocManager", repos = "http://cran.us.r-project.org")
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
 BiocManager::install(c("TCGAbiolinks", "SummarizedExperiment", "edgeR"))
-install.packages(c("pROC", "ggplot2", "dplyr", "tidyr"), repos = "http://cran.us.r-project.org")
+install.packages(c("pROC", "ggplot2", "dplyr", "tidyr")), repos = "http://cran.us.r-project.org")
